6579c7146efeb95d5997d2f252624d127e817d97
Nich87 <touru10286@gmail.com>
2022-03-11 10:37:44 +0900

    some new feature and design fixes.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 M.AutoInit();
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'left'
+    });
+});
+const copyURL = () => {
+    navigator.clipboard.writeText("https://nich87.github.io/");
+    M.toast({
+        html: 'URLをコピーしました！',
+        classes: 'rounded'
+    });
+};
+
+const content_copy = document.getElementById('content-copy');
+const pagetop = document.getElementById('pagetop');
+
+
+content_copy.addEventListener('click', () => copyURL());
+pagetop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
